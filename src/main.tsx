eda1c49593d1a7e78354be0e1dda411420da1d08
antroline <antrolinee@gmail.com>
2026-09-21 01:56:21 +0300

    feat: initialize providers and network settings

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BluxProvider, networks } from '@bluxcc/react'
+import '@fontsource-variable/manrope'
+import '@fontsource-variable/newsreader'
+import './index.css'
+import App from './App.tsx'
+
+const bluxConfig: Parameters<typeof BluxProvider>[0]['config'] = {
+  appId: import.meta.env.VITE_BLUX_APP_ID || 'launchpad-preview',
+  appName: 'Launchpad',
+  networks: [networks.testnet],
+  defaultNetwork: networks.testnet,
+  loginMethods: ['wallet', 'email', 'passkey', 'twitter', 'github', 'gitlab'],
